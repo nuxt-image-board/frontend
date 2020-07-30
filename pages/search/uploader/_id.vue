b629d9e347dd628c3dfc920a5c2f0cd0aed45c88
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-07-30 17:49:05 +0900

    Web: 投稿者別検索ページ作成

---
+++ pages/search/uploader/_id.vue
@@ -0,0 +1,53 @@
+<template>
+  <List
+    :endpoint="endpoint"
+    :notification-title="NotificationTitle"
+    :results-from-props="results"
+    :selected-page-from-props="SelectedPage"
+    :total-page="totalPage"
+    :is-search-page="isSearchPage"
+    :selected-sort-from-props="SelectedSort"
+    :notify-title="pageTitle"
+  />
+</template>
+
+<script>
+import List from '~/components/page/List.vue'
+
+export default {
+  components: {
+    List
+  },
+  async asyncData ({ $axios, $auth, route, error }) {
+    const endpoint = '/search/uploader'
+    const id = isFinite(route.params.id) ? parseInt(route.params.id) : 1
+    const page = isFinite(route.query.page) ? parseInt(route.query.page) : 1
+    const sortNum = isFinite(route.query.sort) ? parseInt(route.query.sort) : 0
+    const order = [0, 2, 4].includes(sortNum) ? 'd' : 'a'
+    const sort = (sortNum <= 1) ? 'd'
+      : (sortNum <= 3) ? 'c'
+        : 'l'
+    const params = { sort, order, page, id }
+    const response = await $axios.get(endpoint, { params })
+    if (response.data.status !== 200) {
+      return error({ statusCode: 404, message: 'err' })
+    }
+    const data = response.data.data
+    return {
+      endpoint,
+      pageTitle: data.title,
+      NotificationTitle: '投稿者から検索 ' + data.title + ' ' + data.count + '件',
+      results: data.imgs,
+      SelectedPage: page,
+      totalPage: data.pages,
+      SelectedSort: sortNum,
+      isSearchPage: true
+    }
+  },
+  head () {
+    return {
+      title: this.pageTitle
+    }
+  }
+}
+</script>
