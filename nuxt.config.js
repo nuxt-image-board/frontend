716ba26327a02251126e042d0c5532ad5c769d84
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-10-29 17:19:26 +0900

    release: α11リリース(7)

--- nuxt.config.js
+++ nuxt.config.js
@@ -286,7 +286,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'http://localhost:5000',
+    baseURL: process.env.API_ENDPOINT,
     browserBaseURL: process.env.API_ENDPOINT,
     proxyHeaders: true
   },
